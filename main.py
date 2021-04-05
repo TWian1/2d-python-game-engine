2971f19538127fceb57358b1bd96db186201bb08
Ian <80492876+TheWolfIan1@users.noreply.github.com>
2021-04-05 19:37:17 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import engine.engine as eng
 import time
 eng.start()
-
-eng.newpixelsprite(2, 2, "0 0 255", "ted")
+eng.newpixelsprite(2, 2, "255 255 255", "lol")
 eng.clearscreen()
 eng.refresh()
-time.sleep(5)
-eng.changepscolor("ted", "255 0 0")
+for a in range(5):
+  time.sleep(0.5)
+  eng.changepscolor("lol", "255 0 0")
+  eng.clearscreen()
+  eng.refresh()
+  time.sleep(0.5)
+  eng.changepscolor("lol", "255 255 255")
+  eng.clearscreen()
+  eng.refresh()
+eng.clearpixelsprites()
 eng.clearscreen()
 eng.refresh()
